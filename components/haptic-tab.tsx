47ea84396edcaa3fcc6c16235d3a55fe0b0c6dfa
jaejoengsin <zhzhqkq0226@gmail.com>
2025-11-15 13:22:31 +0900

    feat 메뉴 진동 추가

--- components/haptic-tab.tsx
+++ components/haptic-tab.tsx
@@ -1,16 +1,22 @@
-import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs';
+import type { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs';
 import { PlatformPressable } from '@react-navigation/elements';
 import * as Haptics from 'expo-haptics';
+import { Platform } from 'react-native';
 
 export function HapticTab(props: BottomTabBarButtonProps) {
   return (
     <PlatformPressable
       {...props}
       onPressIn={(ev) => {
-        if (process.env.EXPO_OS === 'ios') {
-          // Add a soft haptic feedback when pressing down on the tabs.
-          Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+        // iOS와 Android 모두 햅틱 혹은 진동 피드백 제공
+        if (Platform.OS === 'ios') {
+          Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);
+        } else if (Platform.OS === 'android') {
+          // Android의 경우 진동 강도가 다소 강하므로 Medium 정도로 설정
+          Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
         }
+
+        // 원래 onPressIn 이벤트 유지
         props.onPressIn?.(ev);
       }}
     />
