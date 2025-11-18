5b901dbb1baa02cef042bbd002c4448e88b133e5
yongjunTest <yj040106@gmail.com>
2025-11-18 20:27:40 +0900

    feat 마이페이지 구현

---
+++ app/(tabs)/myPage/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack } from "expo-router";
+
+export default function MyPageLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        animation: "slide_from_right",
+      }}
+    >
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+}
+
